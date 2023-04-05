97a2c35a7ef6138bcfd099211dc22e897925d799
bijoymamud <bijoymamud.09@gmail.com>
2023-04-05 21:20:12 +0600

    added ActiveLink to the Navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import ActiveLink from '../ActiveLink/ActiveLink';
 import './Header.css';
 
 const Header = () => {
     return (
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/friends">Friend</Link>
-            <Link to="/posts">Posts</Link>
-            <Link to="/contact">Contact</Link>
+            <ActiveLink to="/">Home</ActiveLink>
+            <ActiveLink to="/about">About</ActiveLink>
+            <ActiveLink to="/friends">Friends</ActiveLink>
+            <ActiveLink to="/posts">Posts</ActiveLink>
+            <ActiveLink to="/contact">Contact</ActiveLink>
         </nav>
     );
 };
